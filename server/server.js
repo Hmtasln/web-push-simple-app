63f8c776687a0a259f199f2a1983a555f6805622
Hmtasln <hamit.c.aslan@gmail.com>
2024-05-14 17:55:35 +0300

    Yes, I am sure I want to commit this change. I will now be prompted to enter a commit message. I will enter a message that describes the change I made. I will then save and close the file.

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@
   //console.log(vapidKeys);
   const subscribeBrave={"endpoint":"https://fcm.googleapis.com/fcm/send/eME5du95QXg:APA91bEeiDjFStvr95EhENlEhhbZ73cObjBBn6BYfvccnPsRZUjLXd-vcUPus1vww2nc9kI4RZpN0nSw4eD5bvmK3LdF7PFTDdrwvlhE4XXOcycR_qidu4IVTClHbkgYGlFgGzeaCHUE","expirationTime":null,"keys":{"p256dh":"BC39g99DjVUrE3iwYzljej2UT3IJDfFLQIcG6yR1-WMiedGEWAMc5Ms41vlwIf9Rdb_L9F4H2oCoy9fSsf61dvI","auth":"Tj9a19SifthPFJFOkqgJnA"}};
 
-/* const subscribeFirefox={"endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABmQhl7yO8wmJkfvq9doZnmQxa5WT2lx7AYP_OGEO9Ga2xTvam905MNvG89iGGEFeP49XWD6bwXkXfW-Ibk_qGLk5CwB-EwI5sv5-GgHusG_YTDwkyXVUKIAzgVl1HoM4QIX0SUhbOwLs5InjEdqaLN51wZeV4fMka7EnqlkltKbz7x-jQ",
+const subscribeFirefox={"endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABmQhl7yO8wmJkfvq9doZnmQxa5WT2lx7AYP_OGEO9Ga2xTvam905MNvG89iGGEFeP49XWD6bwXkXfW-Ibk_qGLk5CwB-EwI5sv5-GgHusG_YTDwkyXVUKIAzgVl1HoM4QIX0SUhbOwLs5InjEdqaLN51wZeV4fMka7EnqlkltKbz7x-jQ",
 "expirationTime":null,
-"keys":{"auth":"r6OHcjfR9mJ4aSZMhegndw","p256dh":"BNYqBVIx1dic8yDNVq7zb5pR86w_jaG20NW1zYVUEg6yud1M_XgGGKEBP6tUXK5a2ppAkSWYF3jtrNWuM6E_cdo"}}; */
+"keys":{"auth":"r6OHcjfR9mJ4aSZMhegndw","p256dh":"BNYqBVIx1dic8yDNVq7zb5pR86w_jaG20NW1zYVUEg6yud1M_XgGGKEBP6tUXK5a2ppAkSWYF3jtrNWuM6E_cdo"}};
 
 webPush.sendNotification(subscribeBrave,"Push natification nasıl yapılır ?");
-/* webPush.sendNotification(subscribeFirefox);  */
+webPush.sendNotification(subscribeFirefox); 
