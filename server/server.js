0e8d1acc39fbf962cf53d6e645cbc4d0fda26b7d
Hmtasln <hamit.c.aslan@gmail.com>
2024-05-14 16:39:44 +0300

    Yes, I am sure I want to commit this change. I will now be prompted to enter a commit message. I will enter a message that describes the change I made. I will then save and close the file.

--- server/server.js
+++ server/server.js
@@ -8,12 +8,7 @@
   console.log(vapidKeys);
   const subscribeBrave={"endpoint":"https://fcm.googleapis.com/fcm/send/e9acwVHBOxY:APA91bF43yvyZpdCt_XdbMS8Vjo51Fveg1PppNzhjPI-4Ghk8icXtLxw8PEceSIfi_ux8NVHa-srMk44lmNv6-D-pZZLPykWEoJlAt3IeuLvc82XeRv4VBbrtWmfNKSbPOdSMtNOn-Vw","expirationTime":null,"keys":{"p256dh":"BFnJjJmJ254dP2j5QuiuM-iKH-Y6TiWRC6j6XX-xkFt3kGLL69XD_VUoj8_AZAM1ihMYLmQc4A_Yed5N7wdD1iw","auth":"TPm4knaFZuUJQUmJoPpZ0w"}};
 
-const subscribeFirefox={
-"endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABmQhl7yO8wmJkfvq9doZnmQxa5WT2lx7AYP_OGEO9Ga2xTvam905MNvG89iGGEFeP49XWD6bwXkXfW-Ibk_qGLk5CwB-EwI5sv5-GgHusG_YTDwkyXVUKIAzgVl1HoM4QIX0SUhbOwLs5InjEdqaLN51wZeV4fMka7EnqlkltKbz7x-jQ",
-"expirationTime":null,
-"keys":{"auth":"r6OHcjfR9mJ4aSZMhegndw",
-"p256dh":"BNYqBVIx1dic8yDNVq7zb5pR86w_jaG20NW1zYVUEg6yud1M_XgGGKEBP6tUXK5a2ppAkSWYF3jtrNWuM6E_cdo"
-}};
+const subscribeFirefox={"endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABmQhl7yO8wmJkfvq9doZnmQxa5WT2lx7AYP_OGEO9Ga2xTvam905MNvG89iGGEFeP49XWD6bwXkXfW-Ibk_qGLk5CwB-EwI5sv5-GgHusG_YTDwkyXVUKIAzgVl1HoM4QIX0SUhbOwLs5InjEdqaLN51wZeV4fMka7EnqlkltKbz7x-jQ","expirationTime":null,"keys":{"auth":"r6OHcjfR9mJ4aSZMhegndw","p256dh":"BNYqBVIx1dic8yDNVq7zb5pR86w_jaG20NW1zYVUEg6yud1M_XgGGKEBP6tUXK5a2ppAkSWYF3jtrNWuM6E_cdo"}};
 
 webPush.sendNotification(subscribeBrave,"Brave bildirim gönderildi.");
 webPush.sendNotification(subscribeFirefox,"Firefox bildirim gönderildi."); 
