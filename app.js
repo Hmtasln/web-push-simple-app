3fa7692b49148705ef2086aee27b75074e3156a7
Hmtasln <hamit.c.aslan@gmail.com>
2024-05-14 14:44:23 +0300

    chore: Update service worker registration and push notification handling

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 window.addEventListener("load", async ()=>{
 
 const subscribeButton=document.querySelector("#subscribeButton");
-
+// Servis Kayıt işlemi
 const sW= await navigator.serviceWorker.register("sw.js");
-console.log("Service Worker =>",sW);
+console.log("Service Worker =>", sW);
 
-subscribeButton.addEventListener("click", async ()=>{
+    subscribeButton.addEventListener("click", async () => {
 
-        const serviceWorker= await navigator.serviceWorker.ready;
-        const clientID= await serviceWorker.pushManager.subscribe({
+            const serviceWorker= await navigator.serviceWorker.ready;
+            const clientID= await serviceWorker.pushManager.subscribe({
 
-            userVisibleOnly:true,
-            applicationServerKey:"BLPnbNuF05YjhOCTjkvpkUhg6kBjbF4XJUTH4LU-mGm5IhVZsi7GxcR9EB4VBmbyLFIWHT7i-sjk5RoGQtRF5E4"
+                userVisibleOnly:true,
+                applicationServerKey:"BLPnbNuF05YjhOCTjkvpkUhg6kBjbF4XJUTH4LU-mGm5IhVZsi7GxcR9EB4VBmbyLFIWHT7i-sjk5RoGQtRF5E4",
 
 
-        });
+            });
 
-        console.log(clientID);
-        console.log(JSON.stringify(clientID));
-        
-   
-});
+            console.log(clientID);
+            console.log(JSON.stringify(clientID));
+            
+    
+    });
 
 });
--- server/server.js
+++ server/server.js
@@ -3,5 +3,23 @@
  const vapidKeys={
     publicKey: 'BLPnbNuF05YjhOCTjkvpkUhg6kBjbF4XJUTH4LU-mGm5IhVZsi7GxcR9EB4VBmbyLFIWHT7i-sjk5RoGQtRF5E4',    
     privateKey: 'u1DT65LWaH1swAr9pxTVZRFOvWg4hQMqLW9iqvjNAn4'
-  }
- console.log(vapidKeys);
+  };
+  webPush.setVapidDetails("mailto:hamit.c.aslan@gmail.com",vapidKeys.publicKey,vapidKeys.privateKey)
+  console.log(vapidKeys);
+  const subscribeBrave={
+  "endpoint":"https://fcm.googleapis.com/fcm/send/dh17tHzWO2Q:APA91bGXWL_wwLsAdSMj78dhYvuH2EG4kX0D_IKiYp6ITvJa5KYf2VFdETWfJBI2gABZEy55LMAa2WvhzuQY2J24tpQTwC2D7H_GMLn1NXHRNdSaiTPvlWY6GHG_oNqFHCx4W_yBp7-C",
+  "expirationTime":null,
+  "keys":{
+    "p256dh":"BAAZHUtr2Qb3YhSuFFDMFbU8FGP0iaro1RhbWxTXQciNQ1zAuo6jJzFRCJs_BDuRO17nYlpRA92XLfbAP-MlHm0",
+  "auth":"Df9HufLdX9qxTCcctPD5Sg"}
+};
+
+const subscribeFirefox={
+"endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABmQhl7yO8wmJkfvq9doZnmQxa5WT2lx7AYP_OGEO9Ga2xTvam905MNvG89iGGEFeP49XWD6bwXkXfW-Ibk_qGLk5CwB-EwI5sv5-GgHusG_YTDwkyXVUKIAzgVl1HoM4QIX0SUhbOwLs5InjEdqaLN51wZeV4fMka7EnqlkltKbz7x-jQ",
+"expirationTime":null,
+"keys":{"auth":"r6OHcjfR9mJ4aSZMhegndw",
+"p256dh":"BNYqBVIx1dic8yDNVq7zb5pR86w_jaG20NW1zYVUEg6yud1M_XgGGKEBP6tUXK5a2ppAkSWYF3jtrNWuM6E_cdo"
+}};
+
+webPush.sendNotification(subscribeBrave);
+webPush.sendNotification(subscribeFirefox);
